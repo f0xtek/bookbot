0b2e6994452a7afcfc53d6d241218a515c8f1a65
Luke Anderson <landerson61@gmail.com>
2023-08-20 11:15:02 +0100

    Print summary info about books
    Iterates through books in a directory and prints summary information for each book,
    such as the number of words and a count of letters in the book.

---
+++ main.py
@@ -0,0 +1,56 @@
+"""
+Provide statistical data about books
+"""
+from pathlib import Path
+
+
+class Book:
+    """
+    A book with a given path
+    """
+    def __init__(self, path):
+        self.path = path
+        self.__content = self.__read_contents()
+
+    def __read_contents(self):
+        """
+        Return the contents of the book
+        """
+        with open(self.path, "r", encoding="utf-8") as content:
+            return content.read()
+
+    def num_words(self):
+        """
+        Count the number of words in the book's content
+        """
+        return len(self.__content.split())
+
+    def num_letters(self):
+        """
+        Return a dictionary containing the numbe rof times a letter appears int he book's content
+        """
+        results = {}
+        for char in self.__content:
+            char = char.lower()
+            if char.isalpha():
+                if char not in results:
+                    results[char] = 1
+                else:
+                    results[char] += 1
+        return results
+
+    def print_report(self):
+        """
+        Print a summary of the book's content
+        """
+        print(f"--- Begin report of {self.path} ---")
+        print(f"{self.num_words()} words found in the document", end="\n\n")
+        for char in sorted(self.num_letters().items(), key=lambda x: x[1], reverse=True):
+            print(f"The '{char[0]}' character was found {char[1]} times")
+        print("--- End report ---", end="\n\n")
+
+
+if __name__ == "__main__":
+    for item in Path("books").iterdir():
+        book = Book(item)
+        book.print_report()
